212b053d5418c55a7cd30c47a7b45f242fd7fcb1
Charles Mercer <c.e.mercer15@gmail.com>
2019-06-25 12:24:39 -0400

    first attempt at dynamically displaying topic buttons did not work

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -28,26 +28,36 @@ var topics = ["sports","emotions","funny"]
           // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
           var a = $("<button>");
           // Adding a class
-          a.addClass("movie");
+          a.addClass("topic");
           // Adding a data-attribute with a value of the movie at index i
           a.attr("data-name", topics[i]);
           // Providing the button's text with a value of the movie at index i
           a.text(topics[i]);
           // Adding the button to the HTML
           $("#buttons-view").append(a);
+          var buttonChoice = $('<button/>').attr({ type: 'button', class: 'topic', value: i });
+          var buttonTopic = ["sports", "emotions", "funny"];
+          buttonChoice.text(buttonTopic[i]);
+          $("#ptrivia").append(buttonChoice);
+
+          var b = $("<button>");
+          b.addClass("emotions");
+          b.attr("data-name", topics[i]);
+          b.text(top)
+
         }
       }
 
       // This function handles events where one button is clicked
-      $("#add-movie").on("click", function(event) {
+      $("#add-topic").on("click", function(event) {
         // event.preventDefault() prevents the form from trying to submit itself.
         // We're using a form so that the user can hit enter instead of clicking the button if they want
         event.preventDefault();
 
         // This line will grab the text from the input box
-        var movie = $("#movie-input").val().trim();
+        var topic = $("#topic-input").val().trim();
         // The movie from the textbox is then added to our array
-        movies.push(movie);
+        topics.push(topic);
 
         // calling renderButtons which handles the processing of our movie array
         renderButtons();
