0b5897e804ad9577f886d6630d0acc1e50ac60ec
Charles Mercer <c.e.mercer15@gmail.com>
2019-06-25 19:01:18 -0400

    dynamic buttons not displaying

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,19 +1,21 @@
+var topics = ["sports", "emotions", "funny"];
+renderButtons();
 // Adding click event listen listener to all buttons
-//$("button").on("click", function () {
-// Grabbing and storing the data-animal property value from the button
-/* var animal = $(this).attr("data-animal");
+$("button").on("click", function () {
+    // Grabbing and storing the data-animal property value from the button
+    var animal = $(this).attr("data-animal");
 
- // Constructing a queryURL using the animal name
- var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-     animal + "&api_key=HrKpFnYYbogbZQ8BpFlOQr0MMkgG1yPI";
+    // Constructing a queryURL using the animal name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=HrKpFnYYbogbZQ8BpFlOQr0MMkgG1yPI";
 
- // Performing an AJAX request with the queryURL
- $.ajax({
-     url: queryURL,
-     method: "GET"
- })*/
+    //Performing an AJAX request with the queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+});
 
-var topics = ["sports", "emotions", "funny"]
 
 function alertTopicName() {
     var topicName = $(this).attr("data-name");
@@ -26,31 +28,22 @@ function renderButtons() {
 
     // Deleting the movie buttons prior to adding new movie buttons
     // (this is necessary otherwise we will have repeat buttons)
-    $("#buttons-view").empty();
+    $("#button-view").empty();
 
     // Looping through the array of movies
+    console.log(topics);
+    console.log(topics.length);
     for (var i = 0; i < topics.length; i++) {
-
+console.log(topics[i]);
         // Then dynamicaly generating buttons for each movie in the array.
         // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adding a class
-        a.addClass("topic");
-        // Adding a data-attribute with a value of the movie at index i
-        a.attr("data-name", topics[i]);
-        // Providing the button's text with a value of the movie at index i
-        a.text(topics[i]);
-        // Adding the button to the HTML
-        $("#buttons-view").append(a);
-        var buttonChoice = $('<button/>').attr({ type: 'button', class: 'topic', value: i });
-        var buttonTopic = ["sports", "emotions", "funny"];
-        buttonChoice.text(buttonTopic[i]);
-        $("#topic-input").append(buttonChoice);
 
-        var b = $("<button>");
-        b.addClass("emotions");
-        b.attr("data-name", topics[i]);
-        b.text(top)
+        // Adding the button to the HTML
+        var buttonChoice = $('<button>').attr({ type: 'button', class: 'topic', "data-name": topics[i] });
+        buttonChoice.text(topics[i]);
+        $("#buttons-view").append(buttonChoice);
+
+console.log(buttonChoice);
 
     }
 }
