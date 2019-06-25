247936d4bbdb19d84c063ea027487876557e0625
Charles Mercer <c.e.mercer15@gmail.com>
2019-06-25 11:59:31 -0400

    develop array topics and dynamic button with jQuery

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,7 +1,7 @@
 // Adding click event listen listener to all buttons
-$("button").on("click", function () {
+//$("button").on("click", function () {
     // Grabbing and storing the data-animal property value from the button
-    var animal = $(this).attr("data-animal");
+   /* var animal = $(this).attr("data-animal");
 
     // Constructing a queryURL using the animal name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -11,35 +11,44 @@ $("button").on("click", function () {
     $.ajax({
         url: queryURL,
         method: "GET"
-    })
-        // After data comes back from the request
-        .then(function (response) {
-            console.log(queryURL);
+    })*/
+       
+var topics = ["sports","emotions","funny"]
+      // Function for displaying movie data
+      function renderButtons() {
 
-            console.log(response);
-            // storing the data from the AJAX request in the results variable
-            var results = response.data;
+        // Deleting the movie buttons prior to adding new movie buttons
+        // (this is necessary otherwise we will have repeat buttons)
+        $("#buttons-view").empty();
 
-            // Looping through each result item
-            for (var i = 0; i < results.length; i++) {
+        // Looping through the array of movies
+        for (var i = 0; i < topics.length; i++) {
 
-                // Creating and storing a div tag
-                var animalDiv = $("<div>");
+          // Then dynamicaly generating buttons for each movie in the array.
+          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+          var a = $("<button>");
+          // Adding a class
+          a.addClass("movie");
+          // Adding a data-attribute with a value of the movie at index i
+          a.attr("data-name", topics[i]);
+          // Providing the button's text with a value of the movie at index i
+          a.text(topics[i]);
+          // Adding the button to the HTML
+          $("#buttons-view").append(a);
+        }
+      }
 
-                // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Rating: " + results[i].rating);
+      // This function handles events where one button is clicked
+      $("#add-movie").on("click", function(event) {
+        // event.preventDefault() prevents the form from trying to submit itself.
+        // We're using a form so that the user can hit enter instead of clicking the button if they want
+        event.preventDefault();
 
-                // Creating and storing an image tag
-                var animalImage = $("<img>");
-                // Setting the src attribute of the image to a property pulled off the result item
-                animalImage.attr("src", results[i].images.fixed_height.url);
+        // This line will grab the text from the input box
+        var movie = $("#movie-input").val().trim();
+        // The movie from the textbox is then added to our array
+        movies.push(movie);
 
-                // Appending the paragraph and image tag to the animalDiv
-                animalDiv.append(p);
-                animalDiv.append(animalImage);
-
-                // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-                $("#gifs-appear-here").prepend(animalDiv);
-            }
-        });
-});   
+        // calling renderButtons which handles the processing of our movie array
+        renderButtons();
+      });
